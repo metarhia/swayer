80cc8bf26486e2248cddfdd2f961ee3465dfdd77
Roman <azotgenum@gmail.com>
2021-11-29 15:33:06 +0200

    Separate errors

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import './lib/core.js';
 import './lib/metacomponent.js';
 import './lib/metastyle.js';
-import './lib/element-binding.js';
-import './lib/event-manager.js';
+import './lib/element.js';
+import './lib/events.js';
 import './lib/reflection.js';
+import './lib/errors.js';
 
 export { default } from './lib/core.js';
